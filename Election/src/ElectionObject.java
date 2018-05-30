32dcaa7875a2a38fdd4569fa82a0348cad526e85
RajK <sekharneupane@gmail.com>
2018-05-30 14:10:40 +0300

    commented the need for counter and maxValue

--- Election/src/ElectionObject.java
+++ Election/src/ElectionObject.java
@@ -1,10 +1,7 @@
 
 import java.util.ArrayList;
 
-
-
 public class ElectionObject {
-
 	
 		private ArrayList<Candidate> listCandidate = new ArrayList<Candidate>();
 		
@@ -15,16 +12,18 @@ public class ElectionObject {
 		public void results() {
 			int maxValue = 0;
 			int counter = 0;
+			//iterating through the candidate list array in order to get the maximum value candidate vote.
 			for(Candidate candidate: listCandidate) {
 				if(maxValue<candidate.getVotes()) {
 					maxValue = candidate.getVotes();
 				}
-			}		
+			}		//iterating through the candidate list array. This time to get the count(numbers) of the maximum votes.
 			for(Candidate candidate: listCandidate) {
 				if(candidate.getVotes()== maxValue) {
 					counter++;
 			}
 			}
+			//if the counter is more than 1 means, there are more than two equal maximum equal votes.
 			if(counter>1) {
 				System.out.print("\nThe election is a tie between the following candidates: ");
 			}
@@ -33,7 +32,7 @@ public class ElectionObject {
 					if(counter > 1) {
 						
 						System.out.print("\n " + candidate.getName() + " " + " " + "(" + maxValue + " votes) ");
-					}else if(counter < 2) {
+					}else if(counter < 2) {// if the counter is less than two which means that there is only one occurrence of maximum vote.
 						
 						System.out.print("\nThe winner is " + candidate.getName() + " with " + maxValue + " votes!");
 					}
